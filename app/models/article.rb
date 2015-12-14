337c50875134735cc7bc1a08a1c03cc2189c2a35
sarang-sharma <sarang.sharma14@gmail.com>
2015-12-15 01:53:09 +0530

    Added user_id to article

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,5 @@
 class Article < ActiveRecord::Base
+
+	belongs_to :user
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151214201129) do
+ActiveRecord::Schema.define(version: 20151214201725) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
+
+  add_index "articles", ["user_id"], name: "index_articles_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
