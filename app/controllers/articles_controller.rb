9ff0ffb1a076d75d6cdc33ca50a67730e9971b73
sarang-sharma <sarang.sharma14@gmail.com>
2015-12-15 01:57:18 +0530

    Added authentication

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,7 @@
 class ArticlesController < ApplicationController
 
 	before_action :find_article, only: [:show]
+	before_action :authenticate_user!, except: [:index, :show]
 
 	def index
 		@articles = Article.all.order("title")
