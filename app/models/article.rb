01d0b10da225ac4a431247d96a1d1c9156a79a4e
sarang-sharma <sarang.sharma14@gmail.com>
2015-12-17 14:23:13 +0530

    Fixed tagging bugs

--- app/models/article.rb
+++ app/models/article.rb
@@ -9,4 +9,13 @@ class Article < ActiveRecord::Base
 
 	acts_as_taggable
 
+	def tag_list
+  		self.tags.map { |t| t.name }.join(", ")
+	end
+
+	def tag_list=(new_value)
+	  tag_names = new_value.split(/\s*,\s+/)
+	  self.tags = tag_names.map { |name| Tag.where('name = ?', name).first or Tag.create(:name => name) }
+	end
+
 end
